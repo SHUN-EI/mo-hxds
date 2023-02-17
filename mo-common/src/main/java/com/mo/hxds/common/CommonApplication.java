4318f3029054f42277981c05000ddfa83aa84466
moweiquan <295597613@qq.com>
2023-02-17 23:41:58 +0800

    代驾平台——后台子模块，公共模块的创建及配置文件配置

---
+++ mo-common/src/main/java/com/mo/hxds/common/CommonApplication.java
@@ -0,0 +1,13 @@
+package com.mo.hxds.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by mo on 2023/2/17
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+
+    }
+}

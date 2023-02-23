4db82a5969a2cfb297beafab8312fe3be641f127
moweiquan <295597613@qq.com>
2023-02-23 15:52:20 +0800

    代驾平台——允许跨域请求配置类

---
+++ mo-common/src/main/java/com/mo/hxds/common/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.mo.hxds.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by mo on 2023/2/23
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600);
+    }
+}

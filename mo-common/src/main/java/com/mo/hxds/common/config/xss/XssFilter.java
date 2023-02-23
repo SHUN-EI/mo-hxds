3f5f48c6c5eb81382a3ab8652d952bb56f55f3cf
moweiquan <295597613@qq.com>
2023-02-23 15:45:52 +0800

    代驾平台——配置XSS过滤器的配置类

---
+++ mo-common/src/main/java/com/mo/hxds/common/config/xss/XssFilter.java
@@ -0,0 +1,29 @@
+package com.mo.hxds.common.config.xss;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by mo on 2023/2/23
+ */
+@WebFilter(urlPatterns = "/*")
+public class XssFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        XssHttpServletRequestWrapper wrapper = new XssHttpServletRequestWrapper(request);
+        filterChain.doFilter(wrapper, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
